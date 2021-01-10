d9ee960c377280c45ee09442d2d22b402ea8e5e6
ArmaanSodhiBoss25 <69388896+ArmaanSodhiBoss25@users.noreply.github.com>
2021-01-10 13:44:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var doggo, happyDoggo;
+var database;
+var foodS, foodStock;
+
+function preload()
+{
+  doggo=loadImage("dog.png")
+  happyDoggo=loadImage("dog2.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
